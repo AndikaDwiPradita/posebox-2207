dfae92483374d26be4e91f4a0553233a6147fef9
Andika Dwi Pradita <andikadwipradita8@gmail.com>
2026-05-25 07:36:17 +0700

    Update script.js

--- script.js
+++ script.js
@@ -298,6 +298,7 @@ for (let i = 0; i < MAX_STRIP; i++) {
     const btn = document.createElement("button");
     btn.className = "retake-x";
     btn.innerHTML = "✕";
+    btn.style.top = `${145 + (i * 570)}px`;
     btn.onclick = () => retakeSlot(i);
     // geser ke frame berikutnya
     
